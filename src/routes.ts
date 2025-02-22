7ea5db763bd4f0da1e72c9b41c91219a30eb1ca9
WebPerformer <webperformerz@gmail.com>
2025-02-21 21:10:31 -0300

    Update dependencies

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,14 @@
-import { Router } from 'express'
-import { UserController } from './controllers/UserController'
-import { authMiddleware } from './middlewares/authMiddleware'
+import { Router } from "express";
+import { UserController } from "./controllers/UserController";
+import { authMiddleware } from "./middlewares/authMiddleware";
 
-const routes = Router()
+const routes = Router();
 
-routes.post('/user', new UserController().create)
-routes.post('/login', new UserController().login)
+routes.post("/signup", new UserController().create);
+routes.post("/signin", new UserController().login);
 
-routes.use(authMiddleware)
+routes.use(authMiddleware);
 
-routes.get('/profile', new UserController().getProfile)
+routes.get("/profile", new UserController().getProfile);
 
-export default routes
+export default routes;
